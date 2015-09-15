c1a6730fdda3bc9e49eb93cbe7194c8f4bc7edfa
Ken Robbins <ken@kenrobbins.com>
2015-09-14 22:45:47 -0700

    Release 0.0.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ rapidjson = Extension(
 
 setup(
     name='python-rapidjson',
-    version='0.0.4',
+    version='0.0.5',
     description='Python wrapper around rapidjson',
     long_description=long_description,
     license='MIT License',
