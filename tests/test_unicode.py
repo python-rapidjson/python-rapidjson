e33ea73337ce8b2bdbad6f09a996c4f077b2ee8a
Ken Robbins <ken@kenrobbins.com>
2015-09-20 11:33:19 -0700

    Add more unicode tests.

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 import rapidjson
 
@@ -9,3 +10,27 @@ def test_unicode_1():
     ju = rapidjson.dumps(u)
     js = rapidjson.dumps(s)
     assert ju == js
+    assert ju == json.dumps(u)
+    assert rapidjson.dumps(u, ensure_ascii=False) == json.dumps(u, ensure_ascii=False)
+
+
+@pytest.mark.unit
+def test_unicode_2():
+    u = '\U0010ffff'
+    s = u.encode('utf-8')
+    ju = rapidjson.dumps(u)
+    js = rapidjson.dumps(s)
+    assert ju == js
+    assert ju == json.dumps(u)
+    assert rapidjson.dumps(u, ensure_ascii=False) == json.dumps(u, ensure_ascii=False)
+
+
+@pytest.mark.unit
+def test_unicode_3():
+    u = 'asdf \U0010ffff \U0001ffff qwert \uffff \u10ff \u00ff \u0080 \u7fff \b\n\r'
+    s = u.encode('utf-8')
+    ju = rapidjson.dumps(u)
+    js = rapidjson.dumps(s)
+    assert ju == js
+    assert ju == json.dumps(u)
+    assert rapidjson.dumps(u, ensure_ascii=False) == json.dumps(u, ensure_ascii=False)
