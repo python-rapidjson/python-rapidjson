7be47901ed2e8599763602f34f7e7113407d48f2
Lele Gaifax <lele@metapensiero.it>
2015-12-04 19:57:21 +0100

    Mention the datetime_mode argument in loads() doc string

--- python-rapidjson/docstrings.h
+++ python-rapidjson/docstrings.h
@@ -1,13 +1,20 @@
 #ifndef DOCSTRINGS_H_
 #define DOCSTRINGS_H_
 
-static const char* rapidjson_module_docstring =
-    "Fast, simple JSON encoder and decoder. Based on RapidJSON C++ library.";
+PyDoc_STRVAR(rapidjson_module_docstring,
+             "Fast, simple JSON encoder and decoder. Based on RapidJSON C++ library.");
 
-static const char* rapidjson_loads_docstring =
-    "loads(s, object_hook=None, use_decimal=False, precise_float=True, allow_nan=True)\n\nDecodes a JSON string into Python object.";
+PyDoc_STRVAR(rapidjson_loads_docstring,
+             "loads(s, object_hook=None, use_decimal=False, precise_float=True,"
+             " allow_nan=True, datetime_mode=None)\n"
+             "\n"
+             "Decodes a JSON string into Python object.\n");
 
-static const char* rapidjson_dumps_docstring =
-    "dumps(obj, skipkeys=False, ensure_ascii=True, allow_nan=True, indent=None, default=None, sort_keys=False, use_decimal=False, max_recursion_depth=2048, datetime_mode=None)\n\nEncodes Python object into a JSON string.";
+PyDoc_STRVAR(rapidjson_dumps_docstring,
+             "dumps(obj, skipkeys=False, ensure_ascii=True, allow_nan=True, indent=None,"
+             " default=None, sort_keys=False, use_decimal=False, max_recursion_depth=2048,"
+             " datetime_mode=None)\n"
+             "\n"
+             "Encodes Python object into a JSON string.");
 
 #endif
