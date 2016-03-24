e81154da21aae805f3bc4a084f11d404f6b1bf88
Lele Gaifax <lele@metapensiero.it>
2016-03-24 19:16:54 +0100

    Better grouping for not precise floats benchmarks

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,9 @@ def pytest_benchmark_group_stats(config, benchmarks, group_by):
     result = {}
     for bench in benchmarks:
         data_kind = bench.param.split('-')[0]
-        result.setdefault("%s: %s" % (data_kind, bench.group), []).append(bench)
+        if 'not precise)' in data_kind:
+            group = result.setdefault("not precise floats: %s" % bench.group, [])
+        else:
+            group = result.setdefault("%s: %s" % (data_kind, bench.group), [])
+        group.append(bench)
     return sorted(result.items())
