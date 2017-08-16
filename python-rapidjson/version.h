d66276d9ec9e6fc95cfa3bd93432ca756d35ab6c
Lele Gaifax <lele@metapensiero.it>
2017-08-16 17:33:03 +0200

    Release 0.1.0

--- python-rapidjson/version.h
+++ python-rapidjson/version.h
@@ -1,3 +1,3 @@
-#define PYTHON_RAPIDJSON_VERSION "0.1.0b4"
+#define PYTHON_RAPIDJSON_VERSION "0.1.0"
 #define PYTHON_RAPIDJSON_AUTHOR "Ken Robbins"
 #define PYTHON_RAPIDJSON_AUTHOR_EMAIL "ken@kenrobbins.com"
