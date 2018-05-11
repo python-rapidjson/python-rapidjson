35f1e7eb6552f2a0e69c50c44205f25e71757f92
Lele Gaifax <lele@metapensiero.it>
2018-05-11 17:47:04 +0200

    Tweak the script so its able to emit a third table targeting the new ensure_ascii benchmark

--- benchmarks/tablize.py
+++ benchmarks/tablize.py
@@ -2,14 +2,14 @@
 # :Project:   python-rapidjson -- reST tables writer tool
 # :Author:    Lele Gaifax <lele@metapensiero.it>
 # :License:   MIT License
-# :Copyright: © 2017 Lele Gaifax
+# :Copyright: © 2017, 2018 Lele Gaifax
 #
 
 # Assume we did::
 #
 #  pytest --benchmark-json=comparison.json --compare-other-engines benchmarks/
 #
-# Read back that JSON outcome and produce two reST tables summarizing the
+# Read back that JSON outcome and produce three reST tables summarizing the
 # comparison.
 #
 
@@ -19,77 +19,48 @@ import sys
 import rapidjson
 
 
-CONTENDERS = 'rapidjson_f,rapidjson_c,rapidjson_nn_f,rapidjson_nn_c,ujson,simplejson,json,yajl'.split(',')
-S_HEADERS = r'``dumps()``\ [1]_,``Encoder()``\ [2]_,``dumps(n)``\ [3]_,``Encoder(n)``\ [4]_,ujson\ [5]_,simplejson\ [6]_,stdlib\ [7]_,yajl\ [8]_'.split(',')
-D_HEADERS = r'``loads()``\ [9]_,``Decoder()``\ [10]_,``loads(n)``\ [11]_,``Decoder(n)``\ [12]_,ujson,simplejson,stdlib,yajl'.split(',')
-
-assert len(CONTENDERS) == len(S_HEADERS) == len(D_HEADERS)
-
-FOOTNOTES = [
-    '``rapidjson.dumps()``',
-    '``rapidjson.Encoder()``',
-    '``rapidjson.dumps(number_mode=NM_NATIVE)``',
-    '``rapidjson.Encoder(number_mode=NM_NATIVE)``',
-    '`ujson 1.35 <https://pypi.python.org/pypi/ujson/1.35>`__',
-    '`simplejson 3.13.2 <https://pypi.python.org/pypi/simplejson/3.13.2>`__',
-    'Python %d.%d.%d standard library ``json``' % sys.version_info[:3],
-    '`yajl 0.3.5 <https://pypi.python.org/pypi/yajl/0.3.5>`__',
-    '``rapidjson.loads()``',
-    '``rapidjson.Decoder()``',
-    '``rapidjson.loads(number_mode=NM_NATIVE)``',
-    '``rapidjson.Decoder(number_mode=NM_NATIVE)``']
-
-Timings = namedtuple('Timings', 'min, max, mean, rounds, median')
 Benchmark = namedtuple('Benchmark', 'group, name, contender')
-SingleValue = namedtuple('SingleValue', CONTENDERS)
 
 
 class Comparison:
+    COMPARE_FIELD = 'min'  # one of min, max, mean, median...
 
-    def __init__(self, fname):
-        self.timings = timings = {}
+    def __init__(self, contenders, benchmarks):
+        self.contenders = contenders
+        self.timings = {}
+        self.load_data(benchmarks)
 
-        with open(fname) as f:
-            benchmark_data = rapidjson.loads(f.read())
-
-        for bm in benchmark_data['benchmarks']:
+    def load_data(self, benchmarks):
+        for bm in benchmarks:
             group = bm['group']
             desc = bm['name'].split('[', 1)[1][:-1]
             contender, name = desc.split('-')
-            if contender == 'stdlib json':
-                contender = 'json'
-            if contender in CONTENDERS:
+            if contender in self.contenders:
                 bmark = Benchmark(group, name, contender)
-                stats = bm['stats']
-                timing = Timings(stats['min'], stats['max'], stats['mean'],
-                                 stats['rounds'], stats['median'])
-                timings[bmark] = timing
+                self.timings[bmark] = bm['stats'][self.COMPARE_FIELD]
 
-    def focalize(self, group, fieldname):
-        values = defaultdict(dict)
+    def normalize_and_summarize(self, group):
+        table = defaultdict(dict)
         for bmark, timing in self.timings.items():
             if bmark.group == group:
-                values[bmark.name][bmark.contender] = getattr(timing, fieldname)
-        return {n: SingleValue(**values[n]) for n in values}
+                table[bmark.name][bmark.contender] = timing
 
-    def normalize_and_summarize(self, group, fieldname, by='rapidjson_f'):
-        result = {}
-        values = self.focalize(group, fieldname)
+        normalized = {}
         sums = defaultdict(float)
-        for name, value in values.items():
-            for c in CONTENDERS:
-                sums[c] += getattr(value, c)
-            result[name] = SingleValue(*[(getattr(value, c) / getattr(value, by))
-                                         for c in CONTENDERS])
-        sum = SingleValue(**sums)
+        by = self.contenders[0]
+        for name, values in table.items():
+            for c in self.contenders:
+                sums[c] += values[c]
+            normalized[name] = [values[c] / values[by] for c in self.contenders]
 
-        return result, SingleValue(*[(getattr(sum, c) / getattr(sum, by))
-                                     for c in CONTENDERS])
+        normalized_sum = [sums[c] / sums[by] for c in self.contenders]
 
-    def tabulate(self, group, fieldname, headers, normalize_by='rapidjson_f'):
-        normalized, sum = self.normalize_and_summarize(group, fieldname, normalize_by)
+        return normalized, normalized_sum
 
-        nc = len(CONTENDERS)
+    def tabulate(self, group, headers):
+        normalized, sum = self.normalize_and_summarize(group)
+
+        nc = len(self.contenders)
         widths = [max((len(n)+4 for n in normalized))] + \
                  [max((len(h) for h in headers))] * nc
 
@@ -103,13 +74,13 @@ class Comparison:
         rowfmt = '|' + '|'.join(cells) + '|'
 
         print(seprow)
-        print(rowfmt.format(*([group] + [headers[i] for i, c in enumerate(CONTENDERS)])))
+        print(rowfmt.format(*([group] + [headers[i]
+                                         for i, c in enumerate(self.contenders)])))
         print(headseprow)
 
         def printrow(name, row):
-            values = [getattr(row, c) for c in CONTENDERS]
-            best = min(values)
-            fvalues = [('**%.2f**' if v==best else '%.2f') % v for v in values]
+            best = min(row)
+            fvalues = [('**%.2f**' if v==best else '%.2f') % v for v in row]
             print(rowfmt.format(*([name] + fvalues)))
             print(seprow)
 
@@ -117,6 +88,77 @@ class Comparison:
             printrow(n, normalized[n])
 
         printrow('overall', sum)
+
+
+def dumps_and_loads(benchmarks):
+    contenders = ('rapidjson_f',
+                  'rapidjson_c',
+                  'rapidjson_nn_f',
+                  'rapidjson_nn_c',
+                  'ujson',
+                  'simplejson',
+                  'stdlib json',
+                  'yajl')
+    s_headers = (r'``dumps()``\ [1]_',
+                 r'``Encoder()``\ [2]_',
+                 r'``dumps(n)``\ [3]_',
+                 r'``Encoder(n)``\ [4]_',
+                 r'ujson\ [5]_',
+                 r'simplejson\ [6]_',
+                 r'stdlib\ [7]_',
+                 r'yajl\ [8]_')
+    d_headers = (r'``loads()``\ [9]_',
+                 r'``Decoder()``\ [10]_',
+                 r'``loads(n)``\ [11]_',
+                 r'``Decoder(n)``\ [12]_',
+                 r'ujson',
+                 r'simplejson',
+                 r'stdlib',
+                 r'yajl')
+
+    assert len(contenders) == len(s_headers) == len(d_headers)
+
+    footnotes = [
+        '.. [1] ``rapidjson.dumps()``',
+        '.. [2] ``rapidjson.Encoder()``',
+        '.. [3] ``rapidjson.dumps(number_mode=NM_NATIVE)``',
+        '.. [4] ``rapidjson.Encoder(number_mode=NM_NATIVE)``',
+        '.. [5] `ujson 1.35 <https://pypi.python.org/pypi/ujson/1.35>`__',
+        '.. [6] `simplejson 3.13.2 <https://pypi.python.org/pypi/simplejson/3.13.2>`__',
+        '.. [7] Python %d.%d.%d standard library ``json``' % sys.version_info[:3],
+        '.. [8] `yajl 0.3.5 <https://pypi.python.org/pypi/yajl/0.3.5>`__',
+        '.. [9] ``rapidjson.loads()``',
+        '.. [10] ``rapidjson.Decoder()``',
+        '.. [11] ``rapidjson.loads(number_mode=NM_NATIVE)``',
+        '.. [12] ``rapidjson.Decoder(number_mode=NM_NATIVE)``']
+
+    comparison = Comparison(contenders, benchmarks)
+    comparison.tabulate('serialize', s_headers)
+    print()
+    comparison.tabulate('deserialize', d_headers)
+    return footnotes
+
+
+def ascii_vs_utf8(benchmarks):
+    engines = (('rapidjson', 'rj'),
+               ('ujson', 'uj'),
+               ('simplejson', 'sj'),
+               ('stdlib json', 'json'))
+    contenders = ['%s %s' % (e[0], k) for e in engines for k in ('ascii', 'utf8')]
+    i = 12
+    s_headers = []
+    footnotes = []
+    for e in engines:
+        i += 1
+        s_headers.append(r'``%s ascii``\ [%d]_' % (e[1], i))
+        footnotes.append('.. [%d] ``%s.dumps(ensure_ascii=True)``' % (i, e[0]))
+        i += 1
+        s_headers.append(r'``%s utf8``\ [%d]_' % (e[1], i))
+        footnotes.append('.. [%d] ``%s.dumps(ensure_ascii=False)``' % (i, e[0]))
+
+    comparison = Comparison(contenders, benchmarks)
+    comparison.tabulate('serialize', s_headers)
+    return footnotes
 
 
 def main():
@@ -127,14 +169,18 @@ def main():
     else:
         fname = 'comparison.json'
 
-    comparison = Comparison(fname)
-    comparison.tabulate('serialize', 'mean', S_HEADERS)
+    with open(fname) as f:
+        benchmarks = rapidjson.loads(f.read())['benchmarks']
+
+    footnotes = []
+
+    footnotes.extend(dumps_and_loads(benchmarks))
     print()
-    comparison.tabulate('deserialize', 'mean', D_HEADERS)
+    footnotes.extend(ascii_vs_utf8(benchmarks))
 
     print()
-    for idx, fn in enumerate(FOOTNOTES, 1):
-        print('.. [%d] %s' % (idx, fn))
+    for fn in footnotes:
+        print(fn)
 
 
 if __name__ == '__main__':
