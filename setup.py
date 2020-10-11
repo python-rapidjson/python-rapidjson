72a92f9540ee44f47210d4d20e7ee965ed4b2e58
Lele Gaifax <lele@metapensiero.it>
2020-10-11 10:50:04 +0200

    Use cibuildwheel 1.6.2, to support Python 3.9
    Also, disable tests for Python 3.4 and Python 3.5.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # :Author:    Ken Robbins <ken@kenrobbins.com>
 # :License:   MIT License
 # :Copyright: © 2015 Ken Robbins
-# :Copyright: © 2016, 2017, 2018, 2019 Lele Gaifax
+# :Copyright: © 2016, 2017, 2018, 2019, 2020 Lele Gaifax
 #
 
 import os.path
@@ -99,10 +99,10 @@ setup(
         'Programming Language :: C++',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python',
     ],
     ext_modules=[Extension('rapidjson', **extension_options)],
