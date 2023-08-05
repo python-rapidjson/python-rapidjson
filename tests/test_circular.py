a46ff5226d3e323353ce20fe893a2bf07f8f45fd
Lele Gaifax <lele@metapensiero.it>
2023-08-05 15:53:41 +0200

    Skip the max recursion depth test on Python 3.12
    See https://discuss.python.org/t/has-sys-setrecursionlimit-behaviour-changed-in-python-3-12b/30205.

--- tests/test_circular.py
+++ tests/test_circular.py
@@ -3,7 +3,7 @@
 # :Author:    John Anderson <sontek@gmail.com>
 # :License:   MIT License
 # :Copyright: © 2015 John Anderson
-# :Copyright: © 2017, 2018, 2020 Lele Gaifax
+# :Copyright: © 2017, 2018, 2020, 2023 Lele Gaifax
 #
 
 import sys
@@ -42,29 +42,39 @@ def test_circular_composite(dumps):
         dumps(dct2)
 
 
-def test_max_recursion_depth(dumps):
-    import sys
+if sys.version_info < (3, 12):
+    # In Python 3.12 and beyond builtin functions are not affected by the recursion limit.
+    #
+    # See the issue https://github.com/python/cpython/issues/107263 and the question
+    # that originated it, then the NEWS introduced by this commit
+    # https://github.com/python/cpython/commit/fa45958450aa3489607daf9855ca0474a2a20878:
+    #
+    #    The recursion limit [that is sys.setrecursionlimit() and sys.getrecursionlimit()]
+    #    now applies only to Python code. Builtin functions do not use the recursion
+    #    limit, but are protected by a different mechanism that prevents recursion from
+    #    causing a virtual machine crash.
 
-    root = child = {}
-    for i in range(500):
-        nephew = {'value': i}
-        child['child'] = nephew
-        child = nephew
+    def test_max_recursion_depth(dumps):
+        root = child = {}
+        for i in range(500):
+            nephew = {'value': i}
+            child['child'] = nephew
+            child = nephew
 
-    dumps(root)
+        dumps(root)
 
-    rl = sys.getrecursionlimit()
+        rl = sys.getrecursionlimit()
 
-    if hasattr(sys, "pyston_version_info"):
-        # In Pyston setrecursionlimit() sets a lower bound on the number of frames,
-        # not an exact count. So set the limit lower:
-        sys.setrecursionlimit(100)
-    else:
-        # Otherwise set it to the exact value:
-        sys.setrecursionlimit(500)
+        if hasattr(sys, "pyston_version_info"):
+            # In Pyston setrecursionlimit() sets a lower bound on the number of frames,
+            # not an exact count. So set the limit lower:
+            sys.setrecursionlimit(100)
+        else:
+            # Otherwise set it to the exact value:
+            sys.setrecursionlimit(500)
 
-    try:
-        with pytest.raises(expected_exception):
-            dumps(root)
-    finally:
-        sys.setrecursionlimit(rl)
+        try:
+            with pytest.raises(expected_exception):
+                dumps(root)
+        finally:
+            sys.setrecursionlimit(rl)
